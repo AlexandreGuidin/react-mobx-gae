5852d21cf15201eaae832c7eccf71680f51659a3
Alexandre Guidin <alexandre.guidin@gmail.com>
2018-12-06 12:03:46 -0200

    yup validation

--- src/utils/Validation.js
+++ src/utils/Validation.js
@@ -2,14 +2,13 @@ export const validateSchema = (schema, values) => {
     if (!schema || !values) return undefined;
 
     try {
-        schema.validateSync(values, {abortEarly: false})
+        schema.validateSync(values, {abortEarly: false});
         return undefined
     } catch (err) {
-         err.inner.map((error) => {
+        return err.inner.map((error) => {
             const errors = {};
             errors[error.path] = error.message;
             return errors
         });
     }
-
 };
--- src/utils/schemas/UserSchema.js
+++ src/utils/schemas/UserSchema.js
@@ -1,6 +1,6 @@
-import yup from "yup";
+import * as yup from "yup";
 
 export const userSchema = yup.object().shape({
     name: yup.string().required('Mandatory'),
-    lastName: yup.string().typeError('Mandatory')
+    lastName: yup.string().required('Mandatory')
 });
